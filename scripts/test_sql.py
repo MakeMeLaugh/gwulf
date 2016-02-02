7619ec62d9940c91a0c8fb7a78b2b54e24f38009
wulf <gwulf88@gmail.com>
2016-02-03 02:03:14 +0300

    up

--- scripts/test_sql.py
+++ scripts/test_sql.py
@@ -3,6 +3,10 @@
 #  __author__ = 'wulf'
 
 import mysql.connector as mysql
+import csv
+
+output_file = './file.xlsx'
+
 
 conn = mysql.connect(user='root', password='123321', database='db_test')
 
@@ -18,6 +22,12 @@ result = list()
 for row in cursor:
     result.append(row)
 
-print("%s\t%s\t%s" % (fields[0], fields[1], fields[2]))
-for i in result:
-    print("%s\t%s\t%s" % (i[0], i[1], i[2]))
+with open(output_file, 'wb') as csvfile:
+    data_writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+    data_writer.writerow(fields)
+    for row in result:
+        data_writer.writerow(row)
+csvfile.close()
+
+cursor.close()
+conn.close()
